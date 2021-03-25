2aa35b6599cf09be561a097391dc46f9799ecc24
Matt Venn <matt@mattvenn.net>
2021-03-25 10:10:46 +0100

    convert to black and white, leave space around pixels to reduce DRC

--- png_2_gds.py
+++ png_2_gds.py
@@ -6,14 +6,21 @@ import sys
 
 # each square will be 1 um
 SCALE=1.0e-6
-PIX_SIZE=20 # in units of scale above
+# min area metal 5 is 4mm
+PIX_SIZE=2.0 # in units of scale above
+# min space is 1.6
+PIX_SPACE=PIX_SIZE # + 1.6
+PIX_GAP = (PIX_SPACE - PIX_SIZE) / 2
 
 # scale the logo down to 40 x 40 squares
-width=40
-height=40
+width=50
+height=50
 
 im = Image.open(sys.argv[1])
 small_im = im.resize((width,height),resample=Image.BILINEAR)
+small_im = im.convert('1') # convert image to black and white
+
+width, height = im.size
 
 # The GDSII file is called a library, which contains multiple cells.
 lib = gdspy.GdsLibrary(unit=SCALE)
@@ -24,9 +31,13 @@ cell = lib.new_cell('LOGO')
 for x in range(width):
     for y in range(height):
         pix = small_im.getpixel((x,y))
+        print(x, y, pix)
         if pix:
             # Create the geometry (a single rectangle) and add it to the cell.
-            rect = gdspy.Rectangle((x * PIX_SIZE, (height-y) * PIX_SIZE), (x * PIX_SIZE + PIX_SIZE, (height-y) * PIX_SIZE + PIX_SIZE), **skywater_metal4)
+            rect = gdspy.Rectangle(
+                (x * PIX_SPACE + PIX_GAP , (height-y) * PIX_SPACE + PIX_GAP),
+                (x * PIX_SPACE + PIX_GAP + PIX_SIZE, (height-y) * PIX_SPACE + PIX_GAP + PIX_SIZE),
+                **skywater_metal5)
             cell.add(rect)
 
 lib.write_gds(sys.argv[2])
